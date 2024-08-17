b307a9ec0d8ee1589d6f67c5f5d80adca4a0cecf
elder <dev.alaskatech@gmail.com>
2024-08-16 20:31:53 -0500

    [ADD] FlexibleParallaxScrollView component

--- app/screens/prices.tsx
+++ app/screens/prices.tsx
@@ -1,22 +1,52 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
+import { StyleSheet, TouchableOpacity, useColorScheme, KeyboardAvoidingView, Platform, Keyboard, Image } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Colors } from "@/constants/Colors";
 import ThemedTextInput from "@/components/ThemedTextInput";
+import CarouselNative from "@/components/CarouselNative";
+import FlexibleParallaxScrollView from "@/components/FlexibleParallaxScrollView";
+
+//@ts-ignore
+import chivasImage from '../../assets/images/chivas.jpg';
 
 const prices = () => {
   return(
-    <ThemedView style={styles.container}>
-        <ThemedTextInput/>
-        <ThemedText>Prices</ThemedText>
-    </ThemedView>
+    <FlexibleParallaxScrollView>
+      <ThemedView style={styles.inputContainer}>
+        <ThemedTextInput
+          style={styles.input}
+          placeholder="Que estas buscando?"
+        />
+        <TouchableOpacity>
+          <Ionicons name="search-circle" size={35} color="#fe961b" />
+        </TouchableOpacity>
+      </ThemedView>
+      <CarouselNative />
+      <ThemedText type="defaultSemiBold">Servicios Frecuentes 🏖️</ThemedText>
+    </FlexibleParallaxScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 8,
+  },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  input: {
+    width: "85%",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 20,
+  },
+  keyboardAvoidingView: {
+    width: "100%",
   },
 });
 
